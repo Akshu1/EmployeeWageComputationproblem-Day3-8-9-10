081dfc0b395a400a3daa722a412149fa1b428135
akshu1 <akshathanagaraj98@gmail.com>
2022-11-20 22:45:32 +0530

    ADD:Calculate Parttime Employee & Wage

---
+++ main/java/com/bridgelabz/employeewagecomputation/Day3ProgramUc3Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProgramUc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ProgramUc3Application.class, args);
+	}
+
+}
---
+++ test/java/com/bridgelabz/employeewagecomputation/Day3ProgramUc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3ProgramUc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
