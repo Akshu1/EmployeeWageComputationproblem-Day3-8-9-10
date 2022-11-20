d6e6ecebe4687dd3ead619069833bb146a75b489
akshu1 <akshathanagaraj98@gmail.com>
2022-11-20 21:35:47 +0530

    Add: Check Employee is present or Absent

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/Day3ProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ProgramsApplication.class, args);
+	}
+
+}
