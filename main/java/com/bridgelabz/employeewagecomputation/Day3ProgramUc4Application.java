0e6d229b678a831a7c60a9280fa460110ee8ecc3
akshu1 <akshathanagaraj98@gmail.com>
2022-11-24 02:20:07 +0530

    Add:Solving USing Switch Case Statement

---
+++ main/java/com/bridgelabz/employeewagecomputation/Day3ProgramUc4Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProgramUc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ProgramUc4Application.class, args);
+	}
+
+}
