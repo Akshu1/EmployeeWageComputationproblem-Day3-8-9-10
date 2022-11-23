ac00ac95e5df3046a70269fe13cd1f7af45112bf
akshu1 <akshathanagaraj98@gmail.com>
2022-11-24 04:33:11 +0530

    ADD:calculating Wages for a Month

---
+++ main/java/com/bridgelabz/employeewagecomputation/Day3ProgramUC5/Day3ProgramUc5Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation.Day3ProgramUC5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProgramUc5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ProgramUc5Application.class, args);
+	}
+
+}
---
+++ test/java/com/bridgelabz/employeewagecomputation/Day3ProgramUC5/Day3ProgramUc5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation.Day3ProgramUC5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3ProgramUc5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
