a38fff8789ddf14076e1ec7c5c4f245a72130e2e
akshu1 <akshathanagaraj98@gmail.com>
2022-11-24 14:18:44 +0530

    Total MonthlyWages using Condition(While)

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/Day3ProgramUc6Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProgramUc6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ProgramUc6Application.class, args);
+	}
+
+}
