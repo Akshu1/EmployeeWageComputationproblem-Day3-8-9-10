fd585797c7e92932e8cbf73bda073c37c4d7eb9d
akshu1 <akshathanagaraj98@gmail.com>
2022-11-27 17:48:47 +0530

    Add:Compute EmployeeWage for Multiple Companies

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/Uc8Application.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewagecomputation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc8Application.class, args);
+	}
+
+}
